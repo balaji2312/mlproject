e9b76e4174ead4a3e24a0d54f489260036150532
balaji2312 <balajir2312@gmail.com>
2023-04-02 17:43:37 +0530

    Model Training done

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,7 +6,8 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
-from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformation,DataTransformationConfig
+from src.components.model_trainer import ModelTrainerConfig,ModelTrainer
 
 @dataclass
 class DataIngestionConfig:
@@ -52,7 +53,12 @@ if __name__=="__main__":
 
     
     data_transformation=DataTransformation()
-    data_transformation.initiate_data_transformation(train_data,test_data)
+    train_arr,test_arr,preprocessor_path= data_transformation.initiate_data_transformation(train_data,test_data)
+
+    model_trainer=ModelTrainer()
+    r2_Score=model_trainer.initiate_model_trainer(train_arr,test_arr,preprocessor_path)
+    print(r2_Score)
+
 
 
 
